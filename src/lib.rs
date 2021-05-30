b3005930146d812c549ccbf3ea74c4d3f4aef74f
Nicholas Brandão <nicholas.computacao@outlook.com>
2021-05-30 22:11:46 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(message: &str);
+}
+
+#[wasm_bindgen]
+pub fn do_sum(x: u32, y: u32) -> u32 {
+    return x + y;
+}
+
+#[wasm_bindgen]
+pub fn show_alert(message: &str) {
+    alert(message);
+}
